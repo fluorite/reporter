05b7956a4334d2119a3568fd3e860ffcd0871cb2
Anton Zakharov <fluorite@mail.ru>
2012-04-16 10:52:13 +0600

    В макет страницы добавлена проверка прав доступа пользователя и отключение вывода ссылок при отсутствии необходимых прав. Также в представление User добавлена проверка прав пользователя на добавление пользователя с отключением вывода соотвествующей ссылки.

--- application/plugins/AccessHandler.php
+++ application/plugins/AccessHandler.php
@@ -21,10 +21,13 @@ class Application_Plugin_AccessHandler extends Zend_Controller_Plugin_Abstract
             $user=Zend_Auth::getInstance()->getStorage()->read();
             // Передача в макет полной информации о текущем пользователе.
             Zend_Layout::getMvcInstance()->getView()->user=$user;
-            if (!Zend_Acl_Factory::getInstance()->hasRole($user->login) ||
-                !Zend_Acl_Factory::getInstance()->has($controller) ||
-                !Zend_Acl_Factory::getInstance()->isAllowed($user->login,$controller,$action))                      
-                $request->setControllerName('index')->setActionName('index');
+            try{
+                if (!Zend_Acl_Factory::getInstance()->isAllowed($user->login,$controller,$action))                      
+                    $request->setControllerName('index')->setActionName('index');
+            }
+            catch (Exception $e){
+                $request->setControllerName('index')->setActionName('index');    
+            }
         }
     }
 }
