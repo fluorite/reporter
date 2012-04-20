b464c3fd64f6683195e45b83eaaaa7da28e83de5
Anton Zakharov <fluorite@mail.ru>
2012-04-20 12:32:37 +0600

    В модель подразделений добавлен метод выборки подразделения, возглавляемого пользователем (незавершено).

--- application/models/DbTable/Department.php
+++ application/models/DbTable/Department.php
@@ -5,6 +5,19 @@ class Application_Model_DbTable_Department extends Zend_Db_Table_Abstract
 
     protected $_name = 'department';
 
-
+    /**
+     * Выборка подразделения, возглавляемого пользователем.
+     * @param int $id идентификатор показателя.
+     * @return bool true, если пользователь руководит каким-либо подразделением. 
+    */
+    public function isManager($userid){
+        $userid = (int)$userid;
+        // Выборка количества подчиненных показателей.
+        $row=$this->fetchRow($this->select()->from(array('report_items'),array('has'=>'count(*)'))->where('parentid=?',$id));
+        if ($row['has'] != 0)
+            return true;
+        else
+            return false;
+    }
 }
 
